2f41258e10c038d26e074bf9921b1452816a6ebd
med-ridha <51748062+med-ridha@users.noreply.github.com>
2020-06-09 15:11:45 +0100

    first

--- index.js
+++ index.js
@@ -161,9 +161,7 @@ bot.on('message', message => {
                         /*for (var i in videos) {
                             console.log(videos[i].title + '\n');
                         }*/
-                        if (!videos[0].url) {
-                            search(songname);
-                        }
+
                         try {
 
 
